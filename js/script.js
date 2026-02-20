7b562f65327d560718dbbedc56611a1758b56a4f
azwamuzannilubis-boop <azwamuzannilubis@gmail.com>
2026-02-20 07:54:07 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
-// ================= REGISTER =================
 function register() {
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
     const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
     const password = document.getElementById("password").value;
 
+    if (!firstName || !lastName || !email || !phone || !password) {
+        alert("Harap isi semua data!");
+        return;
+    }
+
+    localStorage.setItem("firstName", firstName);
+    localStorage.setItem("lastName", lastName);
     localStorage.setItem("email", email);
+    localStorage.setItem("phone", phone);
     localStorage.setItem("password", password);
 
     alert("Pendaftaran berhasil!");
+    window.location.href = "index.html";
 }
 
 // ================= LOGIN =================
